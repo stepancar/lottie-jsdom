a9f459a35a72a04454f55184581684da15a2c13f
Stepan Mikhailiuk <istepancar@gmail.com>
2024-08-09 18:36:32 -0700

    chore(*): add tests

---
+++ tests/index.spec.js
@@ -0,0 +1,19 @@
+const test = require('node:test');
+const assert = require('node:assert');
+
+
+test('can run lottie svg renderer without specifying animation data in node', (t) => {
+
+    const lottie = require('..');
+    const { document } = lottie;
+    const container = document.createElement('div');
+    const animation = lottie.loadAnimation({
+        container,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+    });
+    animation.goToAndStop(0, true);
+
+    assert.strictEqual(animation.renderer.svgElement.outerHTML, '<svg><defs></defs><g></g></svg>');
+});
